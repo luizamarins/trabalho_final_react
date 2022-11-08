32e63c32ae916fac20181bf8398a4ba2f9b25292
luizamarins <luiza.marins@gmail.com>
2022-11-07 22:27:01 -0300

    go

---
+++ src/pages/Cadastro/index.js
@@ -0,0 +1,10 @@
+export 
+
+function handleRegisterProducts() {
+
+    productService.postProducts(product).then(res => {
+        console.log(res);
+    }).catch(error => {
+        console.log(error);
+    })
+}
