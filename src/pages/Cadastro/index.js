e6b4495a1e1450dc856694fde49ac4e152293237
luizamarins <luiza.marins@gmail.com>
2022-11-08 02:18:06 -0300

    página de cadastro

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -1,26 +1,25 @@
-// import React, { useState, useEffect } from 'react';
-// import { Container, Logo, Link, Category, Headers } from './styled.js';
-// import logo1 from '../../assets/img/logo1.jpg';
-// import { ButtonCategory } from '../../components/ButtonCategory/index.js';
-// import productService from '../../services/requests/productService.js'
-// import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Container, Logo, Link, Category, Headers } from './styled.js';
+import logo1 from '../../assets/img/logo1.jpg';
+import { ButtonCategory } from '../../components/ButtonCategory/index.js';
+import productService from '../../services/requests/productService.js'
+import { ProductForm } from '../../components/ProductForm/index.js';
 
-// export function Cadastro() {
+export function Cadastro() {
 
-//    const navigate = useNavigate();
-//    const [product, postProducts] = useState([]);
+   function postProducts(product) {
 
-//    function handleRegisterProducts() {
+      productService.postProducts(product).then(res => {
+            console.log(res);
+         })
+         .catch(error => {
+            console.log(error)
+         })
+   }
 
-//       productService.postProducts(product).then(res => {
-//          console.log(res);
-//       }).catch(error => {
-//          console.log(error);
-//       })
-//    }
-// }
-
-//  // function handleGoProducts() {
-//     //     navigate(`/produto`, {state: {
-//     //     }})
-//     // }
+   return (
+      <Container>
+         <ProductForm props={postProducts} />
+      </Container>
+   )
+}
